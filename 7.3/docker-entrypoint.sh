a39a02e232444073d9e6dff1ba4b816d8275714c
root <root@lohmag.localdomain>
2019-02-14 22:31:25 +0300

    first commit

---
+++ 7.3/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start cron service
+service cron start
+
+# Toggle xdebug
+if [ "false" == "$XDEBUG" ]; then
+    sed -i "s/^/;/" /usr/local/etc/php/conf.d/xdebug.ini
+    sed -i "s/^/;/" /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+fi
+
+exec "$@"
